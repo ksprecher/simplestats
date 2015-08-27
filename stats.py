88617fda4b5a94cb6b03b8623cd9942e18ac1194
Kate <ksprecher@wisc.edu>
2015-08-27 14:28:45 -0500

    not working

--- stats.py
+++ stats.py
@@ -4,6 +4,13 @@ def mean(vals):
 	return total/length
 
 def std(vals):
-	return 1.0
+	n = len(vals)
+	if n == 0:
+		return 0.0
+	mu = mean(vals)
+	var = 0.0
+	for val in vals:
+		val = var + (val-mu)**2
+	return (var/n)**0.5
 
 # print mean([2,4])
