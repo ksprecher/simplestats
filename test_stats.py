3f7e75157372efcd2994cd47bc175e48644dc3dd
Kate <ksprecher@wisc.edu>
2015-08-27 13:27:58 -0500

    added a test of negative numbers

--- test_stats.py
+++ test_stats.py
@@ -5,5 +5,9 @@ def test_mean():
 test_mean()
 
 def test_float_mean():
-        assert(mean({1,2]) == 1.5)
+        assert(mean([1,2]) == 1.5)
 test_float_mean()
+
+def test_neg_mean():
+	assert(mean([-1,1]) == 0)
+test_neg_mean()
