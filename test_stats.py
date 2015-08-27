85395d5c4218099cfb6fb23b8f875a8e93ccc555
Kate <ksprecher@wisc.edu>
2015-08-27 13:40:18 -0500

    changing to nose testing

--- test_stats.py
+++ test_stats.py
@@ -1,13 +1,13 @@
 from stats import mean
 
 def test_mean():
-        assert(mean([2,4]) == 3)
-test_mean()
+        assert_equal(mean([2,4]), 3)
+#test_mean()
 
 def test_float_mean():
         assert(mean([1,2]) == 1.5)
-test_float_mean()
+#test_float_mean()
 
 def test_neg_mean():
 	assert(mean([-1,1]) == 0)
-test_neg_mean()
+#test_neg_mean()
